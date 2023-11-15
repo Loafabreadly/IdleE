e32d1f7dab78438244e8d053ed9cc786d548179b
Brad S <birdgeekthree@gmail.com>
2023-11-15 07:46:35 -0700

    Initial menu

--- core/src/com/github/loafabreadly/MyGdxGame.java
+++ core/src/com/github/loafabreadly/MyGdxGame.java
@@ -1,31 +1,83 @@
 package com.github.loafabreadly;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.github.loafabreadly.Utilities.Constants;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private Stage stage;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(new InputMultiplexer(stage));
+		createMainUI();
 	}
 
+	private void createMainUI() {
+		Skin skin = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
+
+		TextButton companyButton = new TextButton("Create Company", skin);
+		companyButton.setPosition(Gdx.graphics.getWidth() / 2f - companyButton.getWidth() / 2f, Gdx.graphics.getHeight() / 2f - companyButton.getHeight() / 2f ); //Set the button in the middle
+		companyButton.getStyle().up = skin.newDrawable("white", Color.valueOf(Constants.OXFORDBLUE)); //Set the color of the button to our constant
+		companyButton.getStyle().down = skin.newDrawable("white", Color.valueOf(Constants.YINMNBLUE));
+		companyButton.getStyle().over = skin.newDrawable("white", Color.valueOf(Constants.PLATINUM));
+
+		companyButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				showCompanyCreationUI();
+			}
+		});
+
+		stage.addActor(companyButton);
+
+		stage.getRoot().setColor(Color.valueOf(Constants.OXFORDBLUE)); //Set the Background color
+	}
+
+	private void showCompanyCreationUI() {
+		Skin skin = new Skin(Gdx.files.internal("skin/flat-earth-ui.json"));
+
+		Dialog settingsDiag = new Dialog("Settings", skin);
+
+		TextField nameField = new TextField("", skin);
+		settingsDiag.getContentTable().add("Company Name: ");
+		settingsDiag.getContentTable().add(nameField).row();
+
+		Slider moneySlider = new Slider(0, 100, 1, false, skin);
+		settingsDiag.getContentTable().add("Starting Money: ");
+		settingsDiag.getContentTable().add(moneySlider).row();
+
+		CheckBox saveBox = new CheckBox("Save Company?", skin);
+		settingsDiag.getContentTable().add(saveBox).row();
+
+		settingsDiag.button("Finish", true).button("Cancel", false);
+		settingsDiag.setPosition(Gdx.graphics.getWidth() / 2f - settingsDiag.getWidth() / 2f, Gdx.graphics.getHeight() / 2f - settingsDiag.getHeight() / 2f );
+		settingsDiag.setHeight(settingsDiag.getPrefHeight());
+		settingsDiag.setWidth(settingsDiag.getPrefWidth());
+		settingsDiag.toFront();
+
+		stage.addActor(settingsDiag);
+	}
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		stage.dispose();
 	}
 }
---
+++ core/src/com/github/loafabreadly/Utilities/Constants.java
@@ -0,0 +1,17 @@
+package com.github.loafabreadly.Utilities;
+
+import java.awt.*;
+
+public class Constants {
+
+    public final static int fps = 60;
+
+    public final static String RICHBLACK = "#0D1B2A";
+    public final static String OXFORDBLUE = "#1B263B";
+    public final static String YINMNBLUE = "#415A77";
+    public final static String SILVERLAKEBLUE = "#778DA9";
+    public final static String PLATINUM = "#E0E1DD";
+
+}
+
+
